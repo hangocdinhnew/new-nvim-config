8e4755687e19c6377df1350295bfcccf75094caa
hangocdinhnew <dinhndrwnew@gmail.com>
2026-09-01 07:54:27 +0700

    Fix rust-analyzer.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -6,7 +6,7 @@ vim.lsp.config("clangd", {
   },
 })
 
-vim.lsp.config("rust-analyzer", {
+vim.lsp.config("rust_analyzer", {
   settings = {
     ["rust-analyzer"] = {
       cargo = {
@@ -23,5 +23,5 @@ vim.lsp.config("rust-analyzer", {
 vim.lsp.config("neocmake", {})
 
 vim.lsp.enable("clangd")
-vim.lsp.enable("rust-analyzer")
+vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("neocmake")
